70ed2b08616975affdc7cb15b236bcf894a10e05
OHMelin <oliverholstmelin@gmail.com>
2023-09-29 11:40:54 +0200

    Added image scraping

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
 (async () => {
   // Launch the browser and open a new blank page
@@ -7,7 +8,7 @@ import puppeteer from 'puppeteer';
   });
   const page = await browser.newPage();
 
-  await page.goto('https://www.elgiganten.dk/product/hvidevarer/vask-tor/vaskemaskine/samsung-vaskemaskine-ww95ta047ae/185561');
+  await page.goto('https://www.elgiganten.dk/product/outlet/electrolux-vaskemaskine-ew8f8661u1/399017');
   
   const cookies = '.coi-banner__accept';
   await page.waitForSelector(cookies);
@@ -19,6 +20,18 @@ import puppeteer from 'puppeteer';
   console.log(t);
   
   await page.screenshot({ path: 'example.png' });
+
+  let counter = 0;
+  page.on('response', async (response) => {
+    const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
+    if (matches && (matches.length === 2)) {
+      const extension = matches[1];
+      const buffer = await response.buffer();
+      fs.writeFileSync(`images/image-${counter}.${extension}`, buffer, 'base64');
+      counter += 1;
+    }
+  });
+  await page.goto('https://www.elgiganten.dk/product/outlet/electrolux-vaskemaskine-ew8f8661u1/399017');
 
   await browser.close();
 })();
